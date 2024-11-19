613a0107dd5df618cab1a4a7514b2ed34accbd8f
Tegegnwork <teg_law@yahoo.com>
2024-11-18 22:55:17 -0500

    Props State and Handling

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-function AddTodoForm() {
+function AddTodoForm({onAddTodo}) {
+   function handleAddTodo (event){
+    event.preventDefault();
+    const todoTitle = event.target.elements.title.value;
+    console.log(todoTitle);
+    event.target.reset();
+    onAddTodo(todoTitle);
+  }
+  
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
+      
       <label htmlFor="todoTitle">Title</label>
-      <input id="todoTitle" />
+      <input id="todoTitle" type="text" name="title"/>
 
       <button type="submit">Add</button>
     </form>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TodoListItem from "./TodoListItem";
 const todoList = [
   { id: 1, title: "Complete assignment" },
 
@@ -10,8 +11,8 @@ const todoList = [
 function TodoList() {
   return (
     <ul>
-      {todoList.map(function (item) {
-        return <li key={item.id}>{item.title}</li>;
+      {todoList.map( todo => {
+        return<TodoListItem key={todo.id} todo={todo}/>
       })}
     </ul>
   );
