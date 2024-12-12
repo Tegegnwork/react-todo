addb3d909be5c9a260acf91bcfbdd9153552afd9
Tegegnwork <teg_law@yahoo.com>
2024-12-12 02:29:13 -0500

    hooks and argument

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import React, { useState } from "react";
-import TodoList from "./TodoList";
-import AddTodoForm from "./AddTodoForm";
- 
+   
+   // eslint-disable-next-line no-unused-vars
+   import React, { useEffect, useState } from "react";
+    import TodoList from "./TodoList";
+    import AddTodoForm from "./AddTodoForm";
+        function useSemiPersistentState (){
+        const [todoList, setTodoList] = useState(() => {
+        const savedTodoList = localStorage.getItem ('savedTodoList');
+            return  savedTodoList ? JSON.parse (savedTodoList) : [];
+           });
+  useEffect(() => {
+    localStorage.setItem ('savedTodoList',JSON.stringify(todoList))},[todoList]);
+   return [todoList,setTodoList];
+}
 function App() {
-
-  const [todoList, setTodoList] = useState([]); 
+  const [todoList, setTodoList] = useSemiPersistentState();
+  
   function addTodo  (newTodo)  { 
-  setTodoList([...todoList,newTodo]);
+      setTodoList([...todoList,newTodo]);
   };
    
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo = {addTodo} />
       
       <hr />
       <TodoList todoList = {todoList} />
-    </div>
+    </>
   );
 }
 
