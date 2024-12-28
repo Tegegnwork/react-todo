f043e26a893ae2d5183b2ce8b2dece60cca0dc5d
Tegegnwork <teg_law@yahoo.com>
2024-12-27 20:37:20 -0500

    Edit the sign

--- src/InputWithLabel.jsx
+++ src/InputWithLabel.jsx
@@ -7,7 +7,7 @@ function InputWithLabel (props){
   });
   return (
     <>
-        <label htmlFor = 'todoTitle '>{props.children}</label>
+        <label htmlFor = 'todoTitle'>{props.children}</label>
        <input value = {props.todoTitle} 
     onChange = {props.handleTitleChange} 
     id='todoTitle' 
