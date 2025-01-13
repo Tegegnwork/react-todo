0e8018ac0fd59d5f39ec24287dd26417957613d3
Tegegnwork <teg_law@yahoo.com>
2025-01-13 02:32:38 -0500

    asynchronous Data

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,56 @@
    import React, { useEffect, useState } from "react";
     import TodoList from "./TodoList";
     import AddTodoForm from "./AddTodoForm";
-        function App() {
-   const [todoList, setTodoList] = useState([]);
-   const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        new Promise((resolve , reject) => {
-          const savedTodoList = JSON.parse(localStorage.getItem ('savedTodoList'));
-          setTimeout(() => {
-            const object = {
-              data: {
-                todoList : savedTodoList,
-              },
+  
+
+  const App = () => {
+    console.log(import.meta.env)
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+       const fetchData = async() => {
+        const options = {
+         method: 'GET',
+         headers: { Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`},
+       };
+             
+             
+             const apiUrl = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+             console.log(apiUrl);
+             try {
+              const response = await fetch( apiUrl, options);
+              if (!response.ok){
+                const message = `Error: ${response.status}`;
+                throw new Error(message)
+              }
+              const data = await response.json();
+              console.log(data);
+              const todos = data.records.map((record) => {
+                const newTodo = {
+                  id: record.id,
+                title: record.fields.title
+              
+                }
+                return newTodo
+                  
+                   
+                
+                });
+              console.log( todos);
+              setTodoList(todos);
+              setIsLoading(false);
+             } catch(error){
+             console.log(error.message);
+             setIsLoading(false);
+             }
             };
-           resolve(object);
-          },2000);
-        }).then((result) => {
-          setTodoList(result.data.todoList);
-          setIsLoading(false);
-        });
+          
+       
+      useEffect(() => {
+        fetchData();
        },[]);
        useEffect(() => {
         if (!isLoading) {
+          console.log(todoList)
     localStorage.setItem ('savedTodoList',JSON.stringify(todoList))}
   },[todoList, isLoading]); 
     function addTodo  (newTodo)  { 
@@ -31,17 +60,17 @@
   function removeTodo (id){
     const filtertodoList = todoList.filter((todo) => todo.id !== id);
     setTodoList(filtertodoList);
-  }
-   
+  
+  };
   return (
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo = {addTodo} />
-      { isLoading ?
-      (<p>Loading...</p>):
+      { isLoading ?(
+      <p>Loading...</p>):
       (<TodoList onRemoveTodo={removeTodo} todoList = {todoList} />)}
     </>
   );
-}
-
+};
+     
 export default App;
